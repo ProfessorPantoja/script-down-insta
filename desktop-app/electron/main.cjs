5c4e433a7cdbd73aec20026a88628e99dcc111e4
Fabio Pantoja <artpantoja@gmail.com>
2026-02-22 21:58:13 -0300

    fix: change electron scripts to .cjs to support ES modules in vite

--- desktop-app/electron/main.cjs
+++ desktop-app/electron/main.cjs
@@ -10,7 +10,7 @@ function createWindow() {
         height: 700,
         backgroundColor: '#0f172a',
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+            preload: path.join(__dirname, 'preload.cjs'),
             nodeIntegration: false,
             contextIsolation: true
         },
